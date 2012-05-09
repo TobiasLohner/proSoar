bf0f10d38e6204502aa7c49abf8cb0952bf7df9e
Tobias Lohner <tobias@lohner-net.de>
2012-05-09 15:59:30 +0200

    util: add server-side search script
    This is required because cross-site-json-requests are not permitted by browsers.

---
+++ util/bin/public/search.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import cgi
+import cgitb; cgitb.enable()
+import re
+import urllib2
+
+def main():
+
+  form = cgi.FieldStorage()
+
+  m = re.compile('^(([-]?\d*\.?\d*?),([-]?\d*\.?\d*?),([-]?\d*\.?\d*?),([-]?\d*\.?\d*?))$').match(form.getvalue('bbox'))
+  bbox = m.group(1)
+
+  m = re.compile('^(.*)$').match(form.getvalue('q'))
+  q = urllib2.quote(m.group(1))
+
+  url = 'http://nominatim.openstreetmap.org/search/'+q+'?format=json&limit=1' \
+    '&viewbox='+bbox+'&email=info@prosoar.de'
+
+  try:
+    request = urllib2.urlopen(url)
+    reply = request.read()
+
+  except:
+    reply = '[]'
+
+  print "Content-type: text/plain"
+  print
+  print reply
+
+
+if __name__ == '__main__':
+  main()
+
