b8524ea1efa721a56f9d92948000dd00911fe438
Tobias Lohner <tobias@lohner-net.de>
2012-05-16 13:17:29 +0200

    get_height.py: use single srtm files to determine terrain height

--- util/bin/public/get_height.py
+++ util/bin/public/get_height.py
@@ -3,32 +3,48 @@
 import cgi
 import cgitb; cgitb.enable()
 import subprocess
+import re
+import math
+import os
 
 def main():
-  fine_file = "/home/tobias/hillshading/fine.shp"
+  srtm_dir = "/home/tobias/hillshading/fine/tiff"
 
   form = cgi.FieldStorage()
 
   m = re.compile('([-\d.]*)').match(form.getvalue('lon'))
-  lon = m.group(1)
+  lon = float(m.group(1))
 
   m = re.compile('([-\d.]*)').match(form.getvalue('lat'))
-  lat = m.group(1)
+  lat = float(m.group(1))
 
-  if lon > -180 and lon < 180 \
-     and lat > -90 and lat < 90:
-    process = subprocess.Popen(['gdallocationinfo -b 1 -valonly -lifonly -wgs84', \
-      fine_file, '-lon', lon, '-lat', lat])
+  if lon >= -180 and lon <= 180 \
+     and lat >= -60 and lat <= 60:
+
+    file_lat = int(math.ceil((60 - lat)/5))
+    file_lon = int(math.ceil((175 + lon)/5) + 1)
+
+    srtm_file_basename = 'srtm_' + '{0:02d}'.format(file_lon) + '_' +  '{0:02d}'.format(file_lat)
+
+    srtm_file = '/vsizip/' + srtm_dir + '/' + srtm_file_basename + '.zip' + '/' + srtm_file_basename + '.tif' 
+
+    process = subprocess.Popen(['/usr/bin/gdallocationinfo', '-b', '1', '-valonly', '-lifonly', '-wgs84',
+      srtm_file, str(lon), str(lat)], stdout=subprocess.PIPE)
     stdout, stderr = process.communicate()
- 
-    m = re.compile('([-\d.]*)\s([-\d.]*)\s([-\d.]*)').match(stdout)
-    height = m.group(3)
+
+    m = re.compile('([-\d.]*)').match(stdout)
+
+  if not m == None and not m.group(1) == '':
+    height = int(m.group(1))
   else:
     height = -999
   
+  if height == -32768:
+    height = 0
+
   print "Content-type: text/plain"
   print
-  print "{'lon':" + str(lon) + ",'lat':" + str(lat) + ",'height':" + str(height) + "}"
+  print '{"lon":' + str(lon) + ',"lat":' + str(lat) + ',"height":' + str(height) + '}'
 
 
 if __name__ == '__main__':
