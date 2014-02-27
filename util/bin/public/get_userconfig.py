ccaf7b3d46ec27ea215f70f88c2d123dad26f1d8
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-02-27 23:08:35 +0100

    bin/public: Fixed PEP8 issues

--- util/bin/public/get_userconfig.py
+++ util/bin/public/get_userconfig.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 import cgi
-import cgitb; cgitb.enable()
-import os, sys
+import cgitb
+cgitb.enable()
+import os
+import sys
 import GeoIP
 
 app_dir = os.path.abspath(__file__ + '/../../..')
@@ -10,30 +12,35 @@ sys.path.append(os.path.join(app_dir, 'lib'))
 
 from prosoar.userconfig import get_uid_from_cookie, get_user_config_as_json
 
+
 def main():
-  uid = get_uid_from_cookie()
+    uid = get_uid_from_cookie()
 
-  form = cgi.FieldStorage();
+    form = cgi.FieldStorage()
 
-  if form.getvalue('as') == 'js':
-    geoip = GeoIP.open(os.path.join(app_dir, 'storage', 'GeoLiteCity.dat'), GeoIP.GEOIP_STANDARD)
-    location = geoip.record_by_addr(os.environ['REMOTE_ADDR'])
+    if form.getvalue('as') == 'js':
+        geoip = GeoIP.open(
+            os.path.join(app_dir, 'storage', 'GeoLiteCity.dat'),
+            GeoIP.GEOIP_STANDARD
+        )
+        location = geoip.record_by_addr(os.environ['REMOTE_ADDR'])
 
-    print "Content-type: text/html"
-    print
-    print 'var initialSettings = '+get_user_config_as_json(uid)+';'
-    
-    if location:
-      print 'var initialLocation = {lon: '+str(location['longitude'])+', lat: '+str(location['latitude'])+'};'
-    else:
-      print 'var initialLocation = {lon: 10, lat: 50};'
+        print "Content-type: text/html"
+        print
+        print 'var initialSettings = ' + get_user_config_as_json(uid) + ';'
 
-  elif form.getvalue('as') == 'json':
-    print "Content-type: text/html"
-    print
-    print get_user_config_as_json(uid)
+        if location:
+            print 'var initialLocation = {lon: ' + \
+                str(location['longitude']) + ', lat: ' + \
+                str(location['latitude']) + '};'
+        else:
+            print 'var initialLocation = {lon: 10, lat: 50};'
+
+    elif form.getvalue('as') == 'json':
+        print "Content-type: text/html"
+        print
+        print get_user_config_as_json(uid)
 
 
 if __name__ == '__main__':
-  main()
-  
+    main()
--- util/bin/public/search.py
+++ util/bin/public/search.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python
 
 import cgi
-import cgitb; cgitb.enable()
+import cgitb
+cgitb.enable()
 import re
 import urllib2
 
+
 def main():
+    form = cgi.FieldStorage()
 
-  form = cgi.FieldStorage()
+    m = re.compile(
+        '^(([-]?\d*\.?\d*?),([-]?\d*\.?\d*?),([-]?\d*\.?\d*?),([-]?\d*\.?\d*?))$').match(form.getvalue('bbox'))
+    bbox = m.group(1)
 
-  m = re.compile('^(([-]?\d*\.?\d*?),([-]?\d*\.?\d*?),([-]?\d*\.?\d*?),([-]?\d*\.?\d*?))$').match(form.getvalue('bbox'))
-  bbox = m.group(1)
+    m = re.compile('^(.*)$').match(form.getvalue('q'))
+    q = urllib2.quote(m.group(1))
 
-  m = re.compile('^(.*)$').match(form.getvalue('q'))
-  q = urllib2.quote(m.group(1))
+    url = 'http://nominatim.openstreetmap.org/search/' + q + \
+        '?format=json&limit=1&viewbox=' + bbox + '&email=info@prosoar.de'
 
-  url = 'http://nominatim.openstreetmap.org/search/'+q+'?format=json&limit=1' \
-    '&viewbox='+bbox+'&email=info@prosoar.de'
+    try:
+        request = urllib2.urlopen(url)
+        reply = request.read()
 
-  try:
-    request = urllib2.urlopen(url)
-    reply = request.read()
+    except:
+        reply = '[]'
 
-  except:
-    reply = '[]'
-
-  print "Content-type: text/plain"
-  print
-  print reply
+    print "Content-type: text/plain"
+    print
+    print reply
 
 
 if __name__ == '__main__':
-  main()
-
+    main()
