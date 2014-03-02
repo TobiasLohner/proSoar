f546a07cd79d873c122030100158bbb8611e2ec3
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-03-02 20:41:51 +0100

    flask: Converted upload_igc_file.py to Flask Blueprint

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ from babel.core import negotiate_locale
 sys.path.append(os.path.join('util', 'bin'))
 
 from public.get_userconfig import bp as get_userconfig_bp
+from public.upload_igc_file import bp as upload_igc_file_bp
 
 AVAILABLE_LOCALES = ['en', 'de', 'cs']
 
 app = Flask(__name__, static_folder='web', static_url_path='')
 app.register_blueprint(get_userconfig_bp)
+app.register_blueprint(upload_igc_file_bp)
 
 
 @app.route("/")
--- util/bin/public/upload_igc_file.py
+++ util/bin/public/upload_igc_file.py
@@ -1,19 +1,8 @@
-#!/usr/bin/env python2.6
+from flask import Blueprint, request, jsonify
 
-import cgi
-import cgitb
-cgitb.enable()
 import os
 import sys
 import tempfile
-import json
-
-try:  # Windows needs stdio set for binary mode.
-    import msvcrt
-    msvcrt.setmode(0, os.O_BINARY)  # stdin  = 0
-    msvcrt.setmode(1, os.O_BINARY)  # stdout = 1
-except ImportError:
-    pass
 
 app_dir = os.path.abspath(__file__ + '/../../..')
 sys.path.append(os.path.join(app_dir, 'lib'))
@@ -22,17 +11,19 @@ from prosoar.igc.igc_parser import SimpleParser
 from prosoar.igc.analyse_flight import analyse_flight
 from prosoar.igc.trace import for_openlayers
 
+UPLOAD_DIR = os.path.join(app_dir, 'storage', 'temp')
 
+bp = Blueprint('upload_igc_file', __name__)
+
+
+@bp.route('/igc/upload', methods=['POST'])
+@bp.route('/bin/upload_igc_file.py', methods=['POST'])
 def main():
-    upload_dir = os.path.join(app_dir, 'storage', 'temp')
 
-    form = cgi.FieldStorage()
-
-    uploaded_file = save_uploaded_file(form, "igcfile-upload-file", upload_dir)
+    uploaded_file = save_uploaded_file()
 
     if not uploaded_file:
-        reply_failure()
-        sys.exit()
+        return jsonify({'success': False})
 
     flight = {}
 
@@ -45,37 +36,25 @@ def main():
     os.remove(uploaded_file["tempname"])
 
     if flight['trace'] is not None:
-        reply_success(flight)
+        return jsonify({'success': True, 'flight': flight})
     else:
-        reply_failure()
+        return jsonify({'success': False})
 
 
 # save uploaded file to temp file
-def save_uploaded_file(form, form_field, upload_dir):
-    if form_field not in form:
+def save_uploaded_file():
+    file = request.files['igcfile-upload-file']
+    if not file:
         return
 
-    if not os.path.exists(upload_dir):
-        os.makedirs(upload_dir)
-
-    fileitem = form[form_field]
-    if not fileitem.file:
-        return
+    if not os.path.exists(UPLOAD_DIR):
+        os.makedirs(UPLOAD_DIR)
 
     fout = tempfile.NamedTemporaryFile(
-        mode='w+b', delete=False, dir=upload_dir,
+        mode='w+b', delete=False, dir=UPLOAD_DIR,
         suffix='.igc')
 
-    chunks = 0
-
-    # maximum 10*128kb file size
-    while chunks < 10:
-        chunks += 1
-        chunk = fileitem.file.read(1024 * 128)
-        if not chunk:
-            break
-        fout.write(chunk)
-
+    file.save(fout)
     fout.close()
 
     if os.path.getsize(fout.name) == 1024 * 128 * 10 \
@@ -85,22 +64,5 @@ def save_uploaded_file(form, form_field, upload_dir):
 
     return {
         "tempname": fout.name,
-        "filename": os.path.basename(fileitem.filename)
+        "filename": os.path.basename(file.filename)
     }
-
-
-def reply_failure():
-    print "Content-type: text/html"
-    print
-    print '{"success":false}'
-
-
-def reply_success(flight):
-    print "Content-type: text/html"
-    print
-    print '{"success":true,"flight":'
-    print json.dumps(flight)
-    print '}'
-
-if __name__ == '__main__':
-    main()
