a09eb42782ffce1c270b315eb2fd51a960d9c117
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-03-02 20:23:08 +0100

    analysis: Use py-xcsoar library for analysis

--- util/lib/prosoar/igc/analyse_flight.py
+++ util/lib/prosoar/igc/analyse_flight.py
@@ -1,52 +1,42 @@
-#!/usr/bin/env python2.6
-
-import subprocess
 import os
 import sys
-from lxml import etree
+from xcsoar.analysis import analyse_flight as _analyse_flight
 
 app_dir = os.path.abspath(__file__ + '/../../../..')
 sys.path.append(os.path.join(app_dir, 'lib'))
 
 
 def analyse_flight(filename):
-    process = subprocess.Popen(
-        [os.path.join(app_dir, 'bin', 'private', 'AnalyseFlight'),
-         filename], stdout=subprocess.PIPE)
-    stdout, stderr = process.communicate()
+    root = _analyse_flight(filename)
 
     flight = {}
 
-#  tree = etree.fromstring(stdout)
-#  root = tree.getroot()
-    root = etree.fromstring(stdout)
+    if 'contest' in root:
+        child = root['contest']
+        flight[child.get("name")] = parse_contest(child)
 
-    if not root.tag == "analysis":
-        return
-
-    for child in root:
-        if child.tag == "contest":
-            flight[child.get("name")] = parse_contest(child)
-
-        if child.tag == "times":
-            flight['takeoff'] = child.get("takeoff")
-            flight['landing'] = child.get("landing")
+    if 'times' in root:
+        child = root['times']
+        flight['takeoff'] = child.get("takeoff")
+        flight['landing'] = child.get("landing")
 
     return flight
 
 
-def parse_contest(contest):
+def parse_contest(contests):
     traces = {}
 
-    for child in contest:
-        if child.tag == "trace":
-            traces[child.get("name")] = {
-                'trace': parse_trace(child),
-                'speed': float(child.get("speed", 0)),
-                'duration': float(child.get("duration", 0)),
-                'distance': float(child.get("distance", 0)),
-                'score': float(child.get("score", 0))
-            }
+    for contest in contests:
+        name = contest
+        contest = contests[name]
+
+        traces[name] = {
+            'trace': parse_trace(contest),
+            'speed': float(contest.get("speed", 0)),
+            'duration': float(contest.get("duration", 0)),
+            'distance': float(contest.get("distance", 0)),
+            'score': float(contest.get("score", 0))
+        }
 
     return traces
 
