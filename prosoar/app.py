720af74b7eadd865d66702a9ed4bd15533e3f25c
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-03-16 03:58:06 +0100

    flask: Use Flask-Babel extension for locale handling

--- prosoar/app.py
+++ prosoar/app.py
@@ -2,6 +2,7 @@ import sys
 import os.path
 
 from flask import Flask, request, render_template
+from flask.ext.babel import Babel, get_locale
 from babel.core import negotiate_locale
 
 from views.airports import bp as airports
@@ -28,6 +29,14 @@ app.config['STORAGE_FOLDER'] = STORAGE_FOLDER
 app.config['UPLOAD_FOLDER'] = os.path.join(STORAGE_FOLDER, 'temp')
 app.config['USERS_FOLDER'] = os.path.join(STORAGE_FOLDER, 'users')
 
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    preferred = map(lambda x: x.replace('-', '_'),
+                    request.accept_languages.values())
+    return negotiate_locale(preferred, AVAILABLE_LOCALES)
+
 app.register_blueprint(airports, url_prefix='/airports')
 app.register_blueprint(height, url_prefix='/height')
 app.register_blueprint(igc, url_prefix='/igc')
@@ -39,8 +48,4 @@ app.register_blueprint(waypoints, url_prefix='/waypoints')
 
 @app.route("/")
 def hello():
-    preferred = map(lambda x: x.replace('-', '_'),
-                    request.accept_languages.values())
-    locale = negotiate_locale(preferred, AVAILABLE_LOCALES)
-
-    return render_template('index.html.' + locale)
+    return render_template('index.html.' + get_locale())
