619c6c6a71af5a7962f09c3f3bac88a20fcf1426
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-03-16 01:09:12 +0100

    flask: Replaced app.py by Flask-Script manage.py

--- app.py
+++
@@ -1,5 +0,0 @@
-from prosoar.app import app
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
---
+++ manage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from flask.ext.script import Manager
+
+from prosoar.app import app
+
+
+manager = Manager(app)
+
+if __name__ == "__main__":
+    manager.run()
