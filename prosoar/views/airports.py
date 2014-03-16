05e2d660309523249122cb4b6c7bb1679d773eac
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-03-16 05:18:27 +0100

    flask: Fixed /airports/<path> route

--- prosoar/views/airports.py
+++ prosoar/views/airports.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, send_from_directory
+import os.path
+
+from flask import Blueprint, current_app, send_from_directory
 
 bp = Blueprint('airports', __name__)
 
@@ -6,4 +8,6 @@ bp = Blueprint('airports', __name__)
 @bp.route("/<path:filename>")
 def airports(filename):
     return send_from_directory(
-        'storage/airports', filename, mimetype='application/json')
+        os.path.join(current_app.config['STORAGE_FOLDER'], 'airports'),
+        filename, mimetype='application/json'
+    )
