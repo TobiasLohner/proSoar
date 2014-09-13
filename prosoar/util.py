ed4849dfa091117a7ad6d9e90e57960a34708b05
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-09-13 14:02:55 +0200

    util: Removed unused slurp/spew() functions

--- prosoar/util.py
+++ prosoar/util.py
@@ -2,21 +2,6 @@ import subprocess
 import sys
 
 
-def slurp(file):
-    f = open(file, 'r')
-    try:
-        return f.read()
-    finally:
-        f.close()
-
-
-def spew(file, content):
-    f = open(file, 'w')
-    try:
-        f.write(str(content))
-    finally:
-        f.close()
-
 __used_commands = {
     '7zr':       'Please install 7-zip (http://www.7-zip.org/).',
     'wget':      'Please install it using your distribution package manager.'
