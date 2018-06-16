f3a7afbc6374167963c96c3e0c5b5924dedc434c
Fabian Bartschke <fabian@bartschke.de>
2018-06-16 15:56:38 +0200

    map.js: update URL for skylines tile server

--- web_dev/js/proSoar/map.js
+++ web_dev/js/proSoar/map.js
@@ -68,33 +68,12 @@ var MapWindow = new Class({
     osm.events.register('visibilitychanged', this, function() { hillshading.setVisibility(osm.getVisibility()); });
 
     var airspace = new OpenLayers.Layer.XYZ(_("Airspace"),
-      "https://www.skylines-project.org/mapproxy/tiles/1.0.0/airspace/${z}/${x}/${y}.png", {
+      "https://www.skylines.aero/mapproxy/tiles/1.0.0/airspace/${z}/${x}/${y}.png", {
       isBaseLayer: false,
       transparent: true,
       'visibility': true,
       'displayInLayerSwitcher': true
     });
-
-/*
-    var airspace = new OpenLayers.Layer.TMS(_("Airspace"),
-      "airspace/", {
-      type: 'png',
-      getURL: function osm_getTileURL(bounds) {
-        var res = this.map.getResolution();
-        var x = Math.round((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
-        var y = Math.round((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
-        var z = this.map.getZoom();
-        var limit = Math.pow(2, z);
-        if (y < 0 || y >= limit) return OpenLayers.Util.getImagesLocation() + "404.png";
-        else x = ((x % limit) + limit) % limit;
-        return this.url + z + "/" + x + "/" + y + "." + this.type;
-      },
-      isBaseLayer: false,
-      transparent: true,
-      'visibility': true,
-      'displayInLayerSwitcher': true
-    });
-*/
     this.map.addLayer(airspace);
 
     OpenLayers.Feature.Vector.style['default']['strokeWidth'] = '4';
